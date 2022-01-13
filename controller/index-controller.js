ca141d3c89b15af7c1c2da0bcea14e26532460a6
Ivan Geier <vilajr.ivan@gmail.com>
2022-01-13 16:13:32 -0300

    update layout - implement ourChoice movie, flip cards with movies info on catalog

--- controller/index-controller.js
+++ controller/index-controller.js
@@ -4,9 +4,14 @@ const api = new Api();
 // load catalog on screen - request movie data from API and append on screen
 async function populateScreen() {
   let movies = catalog.getCatalog();
-  for (let movie of movies) {
-    let data = await api.findMovieID(movie);
-    appendMovieOnScreen(data);
+  for (let i = 0; i < movies.length; i++) {
+    let data = await api.findMovieID(movies[i]);
+    if (i != 0) {
+      appendMovieOnScreen(data);
+    } else {
+      console.log(data);
+      appendOurChoiceMovie(data);
+    }
   }
 }
 
--- view/index-view.js
+++ view/index-view.js
@@ -1,6 +1,36 @@
-// Show movies on screen
+// Create movie catalog HTML and append on catalog div
 function appendMovieOnScreen(movie) {
   $('.catalog').append(`
-  <img class="poster" src="${movie.Poster}" alt="movie poster" id="${movie.imdbID}">
+  <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img class='d-flex w-100 h-100 poster' src="${movie.Poster}" alt="movie poster">
+    </div>
+    <div class="flip-card-back d-flex flex-column w-10 h-100 align-items-center justify-content-center p-3">
+      <h3 class='text-warning'>${movie.Title}</h3>
+      <p class='text-secondary'>${movie.Plot}</p>
+      <p>${movie.Runtime} | ${movie.Year} | ${movie.Genre}</p>
+      <p> Director: ${movie.Director}</p>
+      <p> ${movie.Actors}</p>
+    </div>
+  </div>
+</div>
+  `);
+}
+
+// Create ourChoice HTML and append to ourChoice div
+function appendOurChoiceMovie(movie) {
+  $('.ourChoice').append(`
+    <img class="w-50 poster me-5" src="${
+      movie.Poster
+    }" alt="movie poster" id="${movie.imdbID}" />
+      <div class="ourChoiceInfo d-flex flex-column justify-content-md-center align-items-center align-items-md-start">
+      <h2 class='text-warning'>${movie.Title.toUpperCase()}</h2>
+      <p class='text-secondary'>${movie.Plot}</p>
+      <p>${movie.Runtime} | ${movie.Year} | ${movie.Genre}</p>
+      <p class='text-secondary'>${movie.Awards}</p>
+      <p> Director: ${movie.Director}</p>
+      <p> ${movie.Actors}</p>
+      </div>
   `);
 }
